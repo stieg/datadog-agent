74b1e2050d464e5f849f99579b25342c27cc27ed
Sylvain Baubeau <lebauce@gmail.com>
2020-09-21 15:30:30 +0200

    Add kitchen_functional_tests target to run tests locally with kitchen

--- test/kitchen/test/integration/dd-security-agent-test/rspec/dd-security-agent-test_spec.rb
+++ test/kitchen/test/integration/dd-security-agent-test/rspec/dd-security-agent-test_spec.rb
@@ -2,10 +2,28 @@ require 'spec_helper'
 
 describe 'successfully run functional test' do
   it 'displays PASS and returns 0' do
-    output = `sudo /tmp/security-agent/testsuite -test.v`
+    output = `sudo /tmp/security-agent/testsuite -test.v 1>&2`
     retval = $?
-    print output
     expect(retval).to eq(0)
-    expect(output).not_to include("FAIL")
+  end
+end
+
+# describe 'successfully run functional test in 32 bits' do
+#   it 'displays PASS and returns 0' do
+#     output = `sudo /tmp/security-agent/testsuite32 -test.v 2>&1`
+#     print output
+#     retval = $?
+#     expect(retval).to eq(0)
+#     expect(output).not_to include("FAIL")
+#   end
+# end
+
+if ! File.exists?('/etc/SuSE-release') and ! File.exists?('/etc/rhsm')
+  describe 'successfully run functional test inside a container' do
+    it 'displays PASS and returns 0' do
+      output = `sudo docker exec -ti docker-testsuite /tmp/security-agent/testsuite -test.v 1>&2`
+      retval = $?
+      expect(retval).to eq(0)
+    end
   end
 end
